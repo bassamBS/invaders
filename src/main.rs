17315fd469ecda53cc63a76d8f0b7212b2a6cf57
bassamBS <bassam-dev@posteo.net>
2022-12-15 20:50:06 +0100

    ✨ Player logic added

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use crossterm::{
     ExecutableCommand,
 };
 use invaders::{
-    frame::{self, new_frame},
+    frame::{self, new_frame, Drawable},
+    player::Player,
     render,
 };
 use rusty_audio::Audio;
@@ -44,13 +45,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
+    let mut player = Player::new();
     // Game loop
     'gameloop: loop {
-        let curr_frame = new_frame();
+        let mut curr_frame = new_frame();
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -60,6 +64,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
         // Draw & render
+        player.draw(&mut curr_frame);
         let _ = tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
